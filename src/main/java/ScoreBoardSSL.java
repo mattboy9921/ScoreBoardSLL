d3097d52648ade4f88aef63004a90fdfd83430d1
mattboy9921 <mattboy9921@aim.com>
2017-09-16 18:24:03 -0400

    Set up test class

--- src/main/java/ScoreBoardSSL.java
+++ src/main/java/ScoreBoardSSL.java
@@ -1,3 +1,15 @@
+import java.util.Random;
+
 public class ScoreBoardSSL {
-// Will be test class
+
+    public static void main (String[] args) {
+
+        Random random = new Random();
+
+        // Create 20 scores and store them in an array
+        Score unsortedScores[] = new Score[20];
+        for (int count = 0; count < 20; count++) {
+            unsortedScores[count] = new Score(random.nextInt(100) + 1);
+        }
+    }
 }
