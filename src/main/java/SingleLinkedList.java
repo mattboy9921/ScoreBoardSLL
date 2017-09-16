02f7658ec1a0af7f2f28e42692b4444dfa095e5b
mattboy9921 <mattboy9921@aim.com>
2017-09-16 17:52:47 -0400

    Rename methods and modify addNode method

--- src/main/java/SingleLinkedList.java
+++ src/main/java/SingleLinkedList.java
@@ -1,8 +1,36 @@
 public class SingleLinkedList {
+
     private Node list;
 
-    public SingleLinkedList(){
-        list = null;
+    public SingleLinkedList() { // Create a new linked list
+        list = null; // Set the list to null, the list is empty
+    }
+
+    public void addNode(Score s) { // Add a new node to the linked list
+        Node newNode = new Node(s); // Create a new node, passing through the score object
+        Node current = list; // Create a node, set it equal to the current linked list
+        if (list == null){ // If the current linked list is empty...
+            list = newNode; // Set the current linked list to the new node (list now has 1 element)
+        }else if (newNode.data.getScore() >= current.data.getScore()){ // Or if the new node's score value is greater than the current linked list's first node's score value...
+            list = newNode; // Set the current linked list to the new node (new node is now the only item in the list)
+            newNode.next = current; // Set the new node's (new node is the whole list at this point) next value to the now *old* linked list's first node
+        }else{ // Or if the new node is less than the current linked list's first node...
+            while (current.next != null && newNode.data.getScore() <= current.next.data.getScore()){ // While there are still more nodes in the list and the new node's score is less than the currently being evaulated node...
+                current = current.next; // Set the current node to the next node in the list (move onto the next node)
+            }
+            if (current.next == null) { // If the next node is null (if we have reached the last node in the linked list)
+                if (countNodes(list) < 10) {
+                    current.next = newNode; // Set the next node to the new node (point the current last node to the new node)
+                }
+            }
+            else { // If the next node ins't null (if we have reached a node that is less than our new node's score value)
+                newNode.next = current.next; // Set the new node's next value to the current node's next value
+                current.next = newNode; // Set the current node's next value to the new node (basically just insert the new node and point it to the next node and the new previous node to it)
+                if (countNodes(list) > 10) {
+                    // remove node
+                }
+            }
+        }
     }
 
     public void addFront(Score s){
@@ -11,7 +39,7 @@ public class SingleLinkedList {
         list = frontNode;
     }
 
-    public void addTail(Score s){
+    public void addTail(Score s) {
         Node tailNode = new Node(s);
         Node current = list;
         if (list == null){
@@ -24,7 +52,8 @@ public class SingleLinkedList {
         }
     }
 
-    public Score bestStudent(){
+
+    public Score topScore(){
         Node current = list.next;
         Score bestStudent = list.data;
         while (current != null){
@@ -44,11 +73,11 @@ public class SingleLinkedList {
         }
     }
 
-    public int countNodesRec(Node list){
+    public int countNodes(Node list){
         if (list.next == null) {
             return 1;
         }else{
-            return countNodesRec(list.next) + 1;
+            return countNodes(list.next) + 1;
         }
     }
 
@@ -65,26 +94,7 @@ public class SingleLinkedList {
         }
     }
 
-    public void addInOrder(Score s){
-        Node newNode = new Node(s);
-        Node current = list;
-        if (list == null){
-            list = newNode;
-        }else if (newNode.data.getScore() < current.data.getScore()){
-            list = newNode;
-            newNode.next = current;
-        }else{
-            while (current.next != null && newNode.data.getScore() > current.next.data.getScore()){
-                current = current.next;
-            }
-            if (current.next == null){
-                current.next = newNode;
-            }else{
-                newNode.next = current.next;
-                current.next = newNode;
-            }
-        }
-    }
+
 
     public Node getList(){
         return list;
