cb77dafbe37a9ac48764be157fbce9d3585f15b6
mattboy9921 <mattboy9921@aim.com>
2017-09-16 20:01:45 -0400

    Rearange methods

--- src/main/java/SingleLinkedList.java
+++ src/main/java/SingleLinkedList.java
@@ -2,32 +2,36 @@ public class SingleLinkedList {
 
     private Node list;
 
-    public SingleLinkedList() { // Create a new linked list
-        list = null; // Set the list to null, the list is empty
+    public SingleLinkedList() {
+        list = null;
     }
 
-    public void addNode(Score s) { // Add a new node to the linked list
-        Node newNode = new Node(s); // Create a new node, passing through the score object
-        Node current = list; // Create a node, set it equal to the current linked list
-        if (list == null){ // If the current linked list is empty...
-            list = newNode; // Set the current linked list to the new node (list now has 1 element)
+    public void addNode(Score s) {
+        Node newNode = new Node(s);
+        Node current = list;
+        if (list == null){
+            list = newNode;
         }
-        else if (newNode.data.getScore() >= current.data.getScore()){ // Or if the new node's score value is greater than the current linked list's first node's score value...
-            list = newNode; // Set the current linked list to the new node (new node is now the only item in the list)
-            newNode.next = current; // Set the new node's (new node is the whole list at this point) next value to the now *old* linked list's first node
-        }
-        else { // Or if the new node is less than the current linked list's first node...
-            while (current.next != null && newNode.data.getScore() <= current.next.data.getScore()){ // While there are still more nodes in the list and the new node's score is less than the currently being evaulated node...
-                current = current.next; // Set the current node to the next node in the list (move onto the next node)
+        else if (newNode.data.getScore() >= current.data.getScore()){
+            list = newNode;
+            newNode.next = current;
+            if (countNodes(list) > 10) {
+                removeLastNode();
             }
-            if (current.next == null) { // If the next node is null (if we have reached the last node in the linked list)
+        }
+        else {
+            while (current.next != null && newNode.data.getScore() <= current.next.data.getScore()){
+                current = current.next;
+            }
+            if (current.next == null) {
                 if (countNodes(list) < 10) {
-                    current.next = newNode; // Set the next node to the new node (point the current last node to the new node)
+                    current.next = newNode;
+                    System.out.println("List less than 10.");
                 }
             }
-            else { // If the next node ins't null (if we have reached a node that is less than our new node's score value)
-                newNode.next = current.next; // Set the new node's next value to the current node's next value
-                current.next = newNode; // Set the current node's next value to the new node (basically just insert the new node and point it to the next node and the new previous node to it)
+            else {
+                newNode.next = current.next;
+                current.next = newNode;
                 if (countNodes(list) > 10) {
                     removeLastNode();
                 }
@@ -102,9 +106,11 @@ public class SingleLinkedList {
 
     public void printLinkedList(){
         Node current = list;
+        int count = 1;
         while (current != null){
-            System.out.println(current.data.toString());
+            System.out.println(count + ": " + current.data.getScore());
             current = current.next;
+            count++;
         }
     }
 
